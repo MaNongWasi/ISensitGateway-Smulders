69313d2e20469b6d18259b3b53d2b7fe96bd4c24
JingjingWang <jingjingwang0051@gmail.com>
2016-12-06 10:57:00 +0100

    change reader file
    reader file is to read data from beacon table and calculate rssi avr in
    1 second

--- isensitgw_rssi_reader/isensitgw_rssi_reader.py
+++ isensitgw_rssi_reader/isensitgw_rssi_reader.py
@@ -1,32 +1,13 @@
+import datetime
 import sys
 api_folder = "/home/pi/ISensitGateway/isensitgwapi/"
 if api_folder not in sys.path:
     sys.path.insert(0, api_folder)
 
-from isensit_sql import *
 from isensit_dynamo import *
+from isensit_sql import *
 
 device = "Acc"
-gateways = ["SMULDERS_GW_001", "SMULDERS_GW_002", "SMULDERS_GW_003", "SMULDERS_GW_004"]
-beacons = ["3699", "13653", "13396"]
-
-def get_limit_time(id):
-    created_at = get_created_at_time(id)
-    if created_at is not None:
-        time = created_at.split(":")
-        second = time[2]
-        if second > 30:
-            minute = int(time[1]) + 1
-            if minute < 10:
-                minute = "0" + str(minute)
-            limit_time = time[0] + ":" + minute + ":00"
-        else:
-    	    limit_time = time[0] + ":30:00"
-        return limit_time
-
-def half_hour():
-    currenttime = datetime.datetime.now()
-    return currenttime.minute == 00 or currenttime.minute == 30
 
 def working():
     currenttime = datetime.datetime.now()
@@ -34,55 +15,56 @@ def working():
     start_t = datetime.datetime.strptime(currentdate + start_time, "%Y-%m-%d %H:%M:%S")
     end_t = datetime.datetime.strptime(currentdate + end_time, "%Y-%m-%d %H:%M:%S")
     today = currenttime.weekday()
-    return currenttime > start_t and currenttime < end_t and today < 5
+    return True
 
-def get_created_at_time(id):
-    return dydb.get_created_at_item(id)
+def current(created_at):
+    currenttime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return currenttime > created_at
 
-def upload_data(id, created_at):    
-    if created_at is not None:
-        returned_items = dydb.get_rssi_item(id, created_at)
-        if returned_items is not None:
-            item_info = {}
-            rssi_max = -1000
-            for item in returned_items:
-            	if 'rssi' in item and 'gatewayID' in item:
-                    rssi = item['rssi']
-                    if rssi > rssi_max:
-                    	rssi_max = rssi
-                    	item_info = item
-            print(item_info)
-	    if "gatewayID" in item_info and "created_at" in item_info and "rssi" in item_info:
-                succeed = dydb.insert_rssi_total(id, item_info)
-        	if succeed:
-                    dydb.delete_rssi_item(id, created_at)
-
-
+def upload_data():
+    try:
+	db.connect_to_db()
+	data = db.read_distinct_acc_beacon_data()
+	if data is None:
+	    print("No data left")
+	else:
+	    for beacon_id in data:
+	        rssi_avr = 0
+		id = beacon_id.values()[0]
+	 	if id is not None:
+		    print id
+		    created_time = db.read_earliest_acc_beacon_data(id)
+		    if created_time is not None:
+			created_at = created_time["created_at"]
+			if current(created_at):
+			    print created_at
+ 		            d = db.read_earliest_acc_beacon_datas(id, created_at)
+		            if d is not None:
+#		    		print len(d)
+		    	        for rssi in d:
+#		 	            print "rssi ", rssi["beacon_rssi"]
+		                    rssi_avr = rssi_avr + rssi["beacon_rssi"]
+ 	                        rssi_avr = rssi_avr / len(d)
+#		    	    print "avr ", rssi_avr
+		    	    db.insert_rssi_data(str(id), rssi_avr, str(created_at))
+		 	    next_data = db.read_next_acc_beacon_data(id, created_at)
+			    if next_data is not None:
+				db.delete_earliest_beacon_data(id, created_at)
+    except Exception as e:
+	print("Error in Aws Sender, reason: ", str(e))
+    else:
+	db.close_db()
 
 try:
     db = ISensitGWMysql()
-#    start_time = db.config_data.get_start_time()
-#    end_time = db.config_data.get_end_time()
-    dydb = ISensitDynamodb(db.gatewayID, db.config_data.get_dynamodb_table(), db.config_data.get_dynamodb_table_person, device)
+    start_time = db.config_data.get_start_time()
+    end_time = db.config_data.get_end_time()
 except Exception as e:
     print("Error in ISensitDynamodb, reason: ", str(e))
 
-
 while True:
     if working():
-	if half_hour():
-            for id in beacons:
-	  	time_end = get_limit_time(id)
-	  	created_at = get_created_at_time(id)
-		print created_at 
-		while created_at is not None and created_at < time_end:
-    		    upload_data(id, created_at)
-    		    created_at = get_created_at_time(id)
-        else:
-	    print("not working hour")
-	    time.sleep(60)
+        upload_data()
     else:
 	print("not working hour")
-
- 
-    
+	time.sleep(60)
