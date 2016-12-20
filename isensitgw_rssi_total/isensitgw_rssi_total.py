b47b55810bd60f17415a3642832d156e0e6dba8d
JingjingWang <jingjingwang0051@gmail.com>
2016-12-20 17:22:12 +0100

    every 30 minutes to upload

--- isensitgw_rssi_total/isensitgw_rssi_total.py
+++ isensitgw_rssi_total/isensitgw_rssi_total.py
@@ -8,7 +8,7 @@ from isensit_dynamo import *
 
 device = "Acc"
 gateways = ["SMULDERS_GW_001", "SMULDERS_GW_002", "SMULDERS_GW_003", "SMULDERS_GW_004"]
-beacons = ["3037"]
+beacons = ["3262", "13591", "5833", "13576", "12931","13305","13320","13500", "13560","13606" ]
 
 def get_limit_time(id):
     created_at = get_created_at_time(id)
@@ -28,10 +28,33 @@ def get_limit_time(id):
 def get_created_at_time(id):
     return dydb.get_created_at_item(id)
 
-def upload_data(id, created_at):    
-    if created_at is not None:
+def upload_data(id, created_at):
+    table_name = "maxrssi_table"     
+    db.connect_to_db()
+
+    if created_at is not None:	
         returned_items = dydb.get_rssi_item(id, created_at)
+	old_json=db.read_last_acc_beacon_data(table_name,id)
+	if old_json is None:
+	    print "no old data"  
+	    row_count = 0
+	    GW1=0
+	    GW2=0
+	    GW3=0
+	    GW4=0
+	else:
+	    row_count = old_json["row_count"]
+	    GW1 = old_json["GW1"]
+	    GW2 = old_json["GW2"]
+	    GW3 = old_json["GW3"]
+	    GW4 = old_json["GW4"]
+	    print "GW1" , GW1
+	    print "GW4" , GW4
+
+		
+	
         if returned_items is not None:
+	    currentt = created_at
             item_info = {}
             rssi_max = -1000
             for item in returned_items:
@@ -39,12 +62,27 @@ def upload_data(id, created_at):
                     rssi = item['rssi']
                     if rssi > rssi_max:
                     	rssi_max = rssi
-                    	item_info = item
-            print(item_info)
-	    if "gatewayID" in item_info and "created_at" in item_info and "rssi" in item_info:
-                succeed = dydb.insert_rssi_total(id, item_info)
-        	if succeed:
-                    dydb.delete_rssi_item(id, created_at)
+			print 'rssi max ', rssi_max 
+                    	item_info = item["gatewayID"]
+			print "item_info", item_info
+	    if item_info == "SMULDERS_GW_001":
+		GW1 = GW1 +1
+	    elif item_info == "SMULDERS_GW_002":
+		GW2 = GW1+1
+	    elif item_info == "SMULDERS_GW_003":
+		GW3 = GW3+1
+	    elif item_info == "SMULDERS_GW_004":
+		GW4 = GW4+1
+		print "GW4 after ", GW4
+
+	    succeed = db.insert_max_rssi(id, currentt, GW1, GW2, GW3, GW4)
+	    db.delete_acc_beacon_data(table_name,id,row_count)
+	    dydb.delete_rssi_item(id, created_at)
+	    db.close_db	 
+#            print(item_info)
+#	    if "gatewayID" in item_info and "created_at" in item_info and "rssi" in item_info:
+#                succeed = dydb.insert_rssi_total(id, item_info)
+#        	if succeed:
 
 
 
@@ -59,19 +97,20 @@ except Exception as e:
 
 while True:
     if db.working():
-	if db.half_hour():
-            for id in beacons:
-	  	time_end = get_limit_time(id)
-	  	created_at = get_created_at_time(id)
-		print created_at 
-		while created_at is not None and created_at < time_end:
-    		    upload_data(id, created_at)
-    		    created_at = get_created_at_time(id)
-        else:
-	    print("not working hour")
-	    time.sleep(60)
+#	if db.half_hour():
+        for id in beacons:
+	    time_end = get_limit_time(id)
+	    created_at = get_created_at_time(id)
+	    print "creted_at", created_at 
+	    while created_at is not None and created_at < time_end:
+    		upload_data(id, created_at)
+    		created_at = get_created_at_time(id)
+#        else:
+#	    print("not working hour")
+#	    time.sleep(60)
     else:
 	print("not working hour")
 
  
     
+
