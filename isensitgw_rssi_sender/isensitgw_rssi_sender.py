09c54682d7e71b4e4c0430dc2aea76d89c751a5a
JingjingWang <jingjingwang0051@gmail.com>
2016-12-20 17:21:24 +0100

    read bunch data and upload

--- isensitgw_rssi_sender/isensitgw_rssi_sender.py
+++ isensitgw_rssi_sender/isensitgw_rssi_sender.py
@@ -17,17 +17,26 @@ def current(created_at):
 def upload_data():
     try:
 	db.connect_to_db()
-	data = db.read_first_data(table_name)
+	data = db.read_rssi_data()
 	if data is None:
 	    print("No data left")
 	else:
-	    row_count = data["row_count"]
-	    id = data["beacon_id"]
-	    rssi_avr = data["rssi"]
-	    created_at = data["created_at"]
-	    print id, rssi_avr, created_at
-    	    dydb.insert_rssi_data(str(id), rssi_avr, created_at)
-  	    db.delete_data(table_name, row_count)
+#	    row = 0
+	    row = dydb.insert_rssi_data(data)
+#	    count = 0
+#	    for d in data:
+#		count = count+1
+#	        row_count = d["row_count"]
+#	        id = d["beacon_id"]
+#	        rssi_avr = d["rssi"]
+#	        created_at = d["created_at"]
+#	        print id, rssi_avr, created_at
+#    	        dydb.insert_rssi_data(str(id), rssi_avr, created_at)
+#		if count > 500:
+#		    count = 0
+#		    row = row_count
+#  		    print "deleteing"
+	    db.delete_rssi_data(row)
     except Exception as e:
 	print("Error in Aws Sender, reason: ", str(e))
     else:
@@ -40,13 +49,13 @@ except Exception as e:
     print("Error in ISensitDynamodb, reason: ", str(e))
 
 while True:
-    if db.working():
-	if db.half_hour():
-            upload_data()
-        else:
-	    print("not half hour")
-	    time.sleep(60)
-    else:
-	print("not working hour")
-   	time.sleep(60)
+#    if db.working():
+#	if db.half_hour():
+     upload_data()
+#        else:
+#	    print("not half hour")
+#	    time.sleep(60)
+#    else:
+#	print("not working hour")
+#  	time.sleep(60)
 
